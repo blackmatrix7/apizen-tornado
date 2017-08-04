edb84a2ba74ddad0cfa0fcab5bab97cfd1fb1736
Matrix <codecolor@outlook.com>
2017-08-04 22:10:25 +0800

    修改json encode方案，支持datetime类型

--- webapi/handler.py
+++ webapi/handler.py
@@ -5,9 +5,36 @@
 # @Site    : 
 # @File    : handler.py
 # @Software: PyCharm
+import json
+from configs import config
+from decimal import Decimal
+from tornado.escape import utf8
+from datetime import datetime, date
 from tornado.web import RequestHandler
+from tornado.util import unicode_type
 
 __author__ = 'blackmatrix'
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+
+    datetime_format = None
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime):
+                return obj.strftime(config.get('DATETIME_FMT'))
+            elif isinstance(obj, date):
+                return obj.strftime(config.get('DATE_FMT'))
+            elif isinstance(obj, Decimal):
+                # 不转换为float是为了防止精度丢失
+                return str(obj)
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return json.JSONEncoder.default(self, obj)
 
 
 class BaseHandler(RequestHandler):
@@ -35,8 +62,38 @@ class ApiBaseHandler(SysBaseHandler):
         self._format = 'json'
         SysBaseHandler.__init__(self, application, request, **kwargs)
 
+    def write(self, chunk):
+        """Writes the given chunk to the output buffer.
+
+        To write the output to the network, use the flush() method below.
+
+        If the given chunk is a dictionary, we write it as JSON and set
+        the Content-Type of the response to be ``application/json``.
+        (if you want to send JSON as a different ``Content-Type``, call
+        set_header *after* calling write()).
+
+        Note that lists are not converted to JSON because of a potential
+        cross-site security vulnerability.  All JSON output should be
+        wrapped in a dictionary.  More details at
+        http://haacked.com/archive/2009/06/25/json-hijacking.aspx/ and
+        https://github.com/facebook/tornado/issues/1009
+        """
+        if self._finished:
+            raise RuntimeError("Cannot write() after finish()")
+        if not isinstance(chunk, (bytes, unicode_type, dict)):
+            message = "write() only accepts bytes, unicode, and dict objects"
+            if isinstance(chunk, list):
+                message += ". Lists not accepted for security reasons; see http://www.tornadoweb.org/en/stable/web.html#tornado.web.RequestHandler.write"
+            raise TypeError(message)
+        if isinstance(chunk, dict):
+            chunk = json.dumps(chunk, cls=CustomJSONEncoder).replace("</", "<\\/")
+            self.set_header("Content-Type", "application/json; charset=UTF-8")
+        chunk = utf8(chunk)
+        self._write_buffer.append(chunk)
+
     def check_xsrf_cookie(self):
         pass
+
 
 
 if __name__ == '__main__':
